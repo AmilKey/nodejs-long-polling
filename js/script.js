d76e7cb25318a1f7961274b6ea183f153317597c
AmilKey <kobzar.ivan1@gmail.com>
2014-01-03 13:54:54 +0400

    add subscribe function

--- js/script.js
+++ js/script.js
@@ -8,3 +8,25 @@ publish.onsubmit = function  () {
 	this.elements.message.value = '';
 	return false;
 };
+
+subscribe();
+
+function subscribe () {
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", "/subscribe", true);
+
+	xhr.onload = function  () {
+		var li = document.createElement('li');
+		li.textContent = this.responseText;
+		messages.appendChild(li);
+
+		subscribe();
+	};
+
+	xhr.onerror = xhr.onabort = function  () {
+		setTimeout(subscribe, 500);
+	};
+
+	xhr.send('');
+}
