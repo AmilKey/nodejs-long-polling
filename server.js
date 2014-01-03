33031add07a9b9a946d1e12f97994a6b1b6edfa0
AmilKey <kobzar.ivan1@gmail.com>
2014-01-03 15:15:58 +0400

    add server.js

--- server.js
+++ server.js
@@ -3,5 +3,23 @@ var fs = require('fs');
 
 http.createServer(function  (req,res) {
 
-	res.end('Hello')
+	switch(req.url){
+		case '/':
+			fs.readFile("index.html", function  (err, data) {
+				if (err) throw err;
+				res.end(data);
+			});
+			break;
+
+		case '/subscribe':
+			break;
+
+		case '/publish':
+			break;
+
+		default:
+			res.statusCode = 404;
+			res.end("Not FOUND");
+	}
+
 }).listen(3000);
